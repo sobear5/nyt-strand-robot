c0c7c3cd1cae024f69c33b2f0d01d968b1b3ee5f
sobear5 <sohormuz@gmail.com>
2025-04-09 14:42:32 +0000

    got web scraping to work

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("hello world")
+import asyncio
+from pyppeteer import launch
 
+async def main():
+    browser = await launch(headless=True, args=['--no-sandbox'])
+    page = await browser.newPage()
+    await page.goto('https://www.nytimes.com/games/strands')
 
-if __name__ == "__main__":
-    main()
+    # Evaluate JS to get all text inside <div class="target-class">
+    div_texts = await page.evaluate('''() => {
+    return Array.from(document.querySelectorAll(".styles-module_strandsBtn__xobCT.styles-module_item__ZXXc7"))
+                .map(el => el.innerText);
+}''')
+
+    print(len(div_texts))
+    await browser.close()
+
+asyncio.run(main())
