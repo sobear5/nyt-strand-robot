23a41cb0c15fa44bf30be4139abe8cb4ce9fe896
sobear5 <sohormuz@gmail.com>
2025-04-15 19:08:59 +0000

    added sorted dictionary list

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import asyncio
 from pyppeteer import launch
+import nltk
+nltk.download('words')
+from nltk.corpus import words
+all_words = words.words()
+all_words.sort()
+filtered_list = [s for s in all_words if len(s) > 3]
+print(len(filtered_list))
+a_list = [s for s in all_words if s[0] == "a"]
+print(a_list)
 
 async def main():
     browser = await launch(headless=True, args=['--no-sandbox'])
@@ -11,7 +20,8 @@ async def main():
     return Array.from(document.querySelectorAll(".styles-module_strandsBtn__xobCT.styles-module_item__ZXXc7"))
                 .map(el => el.innerText);
 }''')
-
+    
+    print(div_texts)
     print(len(div_texts))
     await browser.close()
 
